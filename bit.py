d012e6e5fe881156da5cb1c0c1f1baef65240e7a
pallabchakraborty07 <chpallab07@gmail.com>
2024-08-31 23:19:04 +0530

    Add files via upload

---
+++ bit.py
@@ -0,0 +1,7 @@
+number1=int(input("enter a number"))
+number2=int(input("enter a number"))
+if number1^number2==0:
+    print("the number are equal")
+
+else:
+     print("the numbers are not equal")
